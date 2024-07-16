144c31a20936f86cdad03a19bd0cb336b54cb7b0
KommineniCharan <charank347@gmail.com>
2024-07-16 09:03:22 +0530

    React

---
+++ react1/src/One.jsx
@@ -0,0 +1,13 @@
+import { React, Component } from "react";
+
+class One extends Component {
+  render() {
+    return (
+      <>
+        <h2>React Class Component</h2>
+      </>
+    );
+  }
+}
+
+export default One;
---
+++ react1/src/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let Test = () => {
+  return (
+    <React.Fragment>
+      <h2>React Functional Component Test</h2>
+    </React.Fragment>
+  );
+};
+
+export default Test;
